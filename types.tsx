d383d4c80aeb8f00be9fb7be948ef59db2792935
Sami EL KATEB <sami.elkateb@gmail.com>
2022-01-01 14:22:24 +0100

    Refactoring

--- types.tsx
+++ types.tsx
@@ -9,15 +9,65 @@ export type javascriptCodeType =
 	| 'linebreak'
 	| 'variable';
 
-export type yamlCodeType = 'string' | 'object' | 'variable' | 'tab' | 'linebreak';
+export type yamlCodeType =
+	| 'string'
+	| 'object'
+	| 'variable'
+	| 'tab'
+	| 'linebreak';
 export type indexAndLengthArray = { index: number; length: number }[];
 
-export type answerType = 'text' | 'javascript' | 'terminal' | 'yaml'
+export type answerType = 'text' | 'javascript' | 'terminal' | 'yaml';
 
 export interface toggleButton {
 	isActive: boolean;
 	isButtonActive: boolean;
 	isSwipeActive: boolean;
-	toggleButtonHandler : (override?:boolean)=>void;
-	toggleSwipeHandler : (override?:boolean)=>void;
+	toggleButtonHandler: (override?: boolean) => void;
+	toggleSwipeHandler: (override?: boolean) => void;
 }
+
+export type iconNames =
+	| 'close'
+	| 'question'
+	| 'check'
+	| 'flag'
+	| 'calendar'
+	| 'trophy'
+	| 'curved-flag';
+export type sizes = 'large' | 'med' | 'small';
+
+import { BottomTabScreenProps } from '@react-navigation/bottom-tabs';
+import {
+	CompositeScreenProps,
+	NavigatorScreenParams,
+} from '@react-navigation/native';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+
+declare global {
+	namespace ReactNavigation {
+		interface RootParamList extends RootStackParamList {}
+	}
+}
+
+export type RootStackParamList = {
+	Root: NavigatorScreenParams<RootTabParamList> | undefined;
+	Modal: undefined;
+	Study: undefined;
+	NotFound: undefined;
+};
+
+export type RootStackScreenProps<Screen extends keyof RootStackParamList> =
+	NativeStackScreenProps<RootStackParamList, Screen>;
+
+export type RootTabParamList = {
+	TabOne: undefined;
+	TabTwo: undefined;
+	TabThree: undefined;
+};
+
+export type RootTabScreenProps<Screen extends keyof RootTabParamList> =
+	CompositeScreenProps<
+		BottomTabScreenProps<RootTabParamList, Screen>,
+		NativeStackScreenProps<RootStackParamList>
+	>;
